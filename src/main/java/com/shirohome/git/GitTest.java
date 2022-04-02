635467c01b02ffb1d7f02e766c8157fe422949e3
ShiroZh <102797367+ShiroZh@users.noreply.github.com>
2022-04-02 15:41:15 +0800

    Update GitTest.java

--- src/main/java/com/shirohome/git/GitTest.java
+++ src/main/java/com/shirohome/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hot-fix git!");
         System.out.println("push test");
         System.out.println("pull test!");
+        System.out.println("牛牛牛");
 
     }
 }
